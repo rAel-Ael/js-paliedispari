1049abf7dd97cfe5bfed2ea4e81608ac34dff69e
rAel-Ael <alediru9267@gmail.com>
2026-03-31 19:10:11 +0200

    es1 fatto

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function isPalindromo(parola) {
     return parola == parola.split('').reverse().join('');
 }
 
-
+const parolaUtente = prompt("inserisci parola:");
+if (isPalindromo(parolaUtente)) {
+    console.log("e' palindroma");
+} else {
+    console.log("non e' palindroma");
+}
 
 //Pari e Dispari
